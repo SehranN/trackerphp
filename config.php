f642518b5550cc0a34992ab6409687b5e977f977
Sehran Noor <124164629+SehranN@users.noreply.github.com>
2023-07-23 04:13:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ $host       = getenv('DB_HOST');
 $username   = getenv('DB_USERNAME');
 $password   = getenv('DB_PASSWORD');
 $db_name     = getenv('DB_DATABASE');
+$sslcert = "DigiCertGlobalRootCA.crt.pem";
