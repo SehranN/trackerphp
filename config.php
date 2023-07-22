24202bfa136b79ad023c8336b165c0f5df16d50d
Sehran Noor <124164629+SehranN@users.noreply.github.com>
2023-07-23 04:02:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Belal
- * Date: 12/08/16
- * Time: 7:58 PM
- */
 
-define('DB_USERNAME', 'sehranN');
-define('DB_PASSWORD', 'Qwerty123');
-define('DB_HOST', 'trackernoor.mysql.database.azure.com');
-define('DB_NAME', 'trackerschema');
+$host       = getenv('DB_HOST');
+$username   = getenv('DB_USERNAME');
+$password   = getenv('DB_PASSWORD');
+$db_name     = getenv('DB_DATABASE');
